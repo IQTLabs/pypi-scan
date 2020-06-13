35a5684f77b9b762697630af02cbe937c2568e9c
jspeed-meyers <johnmeyersster@gmail.com>
2020-06-13 13:56:11 -0700

    Use black formatting

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ import collections
 import os
 import time
 
-import urllib.request
 import json
+import urllib.request
 
 from bs4 import BeautifulSoup
 import Levenshtein
@@ -28,11 +28,12 @@ import jsontree
 
 # Key constants
 TOP_N = 50  # number of top packages to examine
-MAX_DISTANCE = 1 # Edit distance threshold to determine typosquatting status
-MIN_LEN_PACKAGE_NAME = 5 # Minimum length of package name to be included for analysis
+MAX_DISTANCE = 1  # Edit distance threshold to determine typosquatting status
+MIN_LEN_PACKAGE_NAME = 5  # Minimum length of package name to be included for analysis
 
-def getAllPackages(page='https://pypi.org/simple/'):
-	""" Download simple list of pypi package names
+
+def getAllPackages(page="https://pypi.org/simple/"):
+    """ Download simple list of pypi package names
 
 	pypi.org/simple conveniently lists all the names of current
 	packages. This function scrapes that listing and then places
@@ -43,26 +44,26 @@ def getAllPackages(page='https://pypi.org/simple/'):
 	package_names: a list of package names on pypi
 	"""
 
-	# Retrieve package name listing data from pypy
-	pypi_package_page = requests.get(page)
+    # Retrieve package name listing data from pypy
+    pypi_package_page = requests.get(page)
 
-	# Get timestamp
-	current_timestamp = time.time()
+    # Get timestamp
+    current_timestamp = time.time()
 
-	# Convert html to easily digestible format
-	soup = BeautifulSoup(pypi_package_page.text, 'html.parser')
+    # Convert html to easily digestible format
+    soup = BeautifulSoup(pypi_package_page.text, "html.parser")
 
-	# Store package names in list
-	package_names = []
-	for elem in soup.find_all('a'):  # Find all <a> tags
-		package_names.append(elem.string)  # Get string inside a tag
+    # Store package names in list
+    package_names = []
+    for elem in soup.find_all("a"):  # Find all <a> tags
+        package_names.append(elem.string)  # Get string inside a tag
 
-	# Return timestamp and package name list
-	return current_timestamp, package_names
+    # Return timestamp and package name list
+    return current_timestamp, package_names
 
 
 def getTopPackages(top_n=TOP_N, stored=False):
-	""" Identify top packages by download count on pypi
+    """ Identify top packages by download count on pypi
 
 	A friendly person has already provided an occasionally
 	updated JSON feed to enable this program to build a list
@@ -79,30 +80,32 @@ def getTopPackages(top_n=TOP_N, stored=False):
 	--top_packages: dict with top packages
 	"""
 
-	if stored: # Get stored data
-		with open('top_packages_may_2020.json', 'r') as f:
-   			data = json.load(f)
-	else: # Get json data for top pypi packages from website
-		top_packages_url = "https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.json"
-		with urllib.request.urlopen(top_packages_url) as url: #nosec
-			data = json.loads(url.read().decode())
+    if stored:  # Get stored data
+        with open("top_packages_may_2020.json", "r") as f:
+            data = json.load(f)
+    else:  # Get json data for top pypi packages from website
+        top_packages_url = (
+            "https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.json"
+        )
+        with urllib.request.urlopen(top_packages_url) as url:  # nosec
+            data = json.loads(url.read().decode())
 
-	# Make JSON data easy to navigate
-	json_data = jsontree.jsontree(data)
+    # Make JSON data easy to navigate
+    json_data = jsontree.jsontree(data)
 
-	# Place top_n packages in dict, where key is package
-	# name and value is rank
-	top_packages = {}
-	for i in range(0, top_n):
-		package_info = json_data.rows[i]
-		package_name = package_info['project']
-		top_packages[package_name] = i + 1
+    # Place top_n packages in dict, where key is package
+    # name and value is rank
+    top_packages = {}
+    for i in range(0, top_n):
+        package_info = json_data.rows[i]
+        package_name = package_info["project"]
+        top_packages[package_name] = i + 1
 
-	return top_packages
+    return top_packages
 
-def filterByPackageNameLen(package_list,
-	                       min_len=MIN_LEN_PACKAGE_NAME):
-	"""
+
+def filterByPackageNameLen(package_list, min_len=MIN_LEN_PACKAGE_NAME):
+    """
 	Filter out package names whose length in characters is
 	greater than a specified minimum length
 
@@ -114,18 +117,18 @@ def filterByPackageNameLen(package_list,
 	--filtered_package_list: filtered list of package names
 	"""
 
-	# Loop thru packages and add package if name's
-	# length is greater than or equal to specified min length
-	filtered_package_list = []
-	for package in package_list:
-		if len(package) >= min_len:
-			filtered_package_list.append(package)
+    # Loop thru packages and add package if name's
+    # length is greater than or equal to specified min length
+    filtered_package_list = []
+    for package in package_list:
+        if len(package) >= min_len:
+            filtered_package_list.append(package)
 
-	return filtered_package_list
+    return filtered_package_list
 
-def distanceCalculations(top_package, all_packages, 
-	                     max_distance=MAX_DISTANCE):
-	""" Find all packages within a defined edit distance
+
+def distanceCalculations(top_package, all_packages, max_distance=MAX_DISTANCE):
+    """ Find all packages within a defined edit distance
 
 	INPUTS:
 	--top_package: package name to perform comparison
@@ -136,28 +139,28 @@ def distanceCalculations(top_package, all_packages,
 	--close_package_names: list of potential typosquatters
 	"""
 
-	# Empty list to store similar package names
-	close_package_names = []
+    # Empty list to store similar package names
+    close_package_names = []
 
-	# Loop thru all package names
-	for package in all_packages:
+    # Loop thru all package names
+    for package in all_packages:
 
-		# Skip if the package IS the same as top_package
-		if package == top_package:
-			continue
+        # Skip if the package IS the same as top_package
+        if package == top_package:
+            continue
 
-		# Calculate distance
-		distance = Levenshtein.distance(top_package, package)
+        # Calculate distance
+        distance = Levenshtein.distance(top_package, package)
 
-		# If distance is sufficiently close, add to list
-		if distance <= max_distance:
-			close_package_names.append(package)
+        # If distance is sufficiently close, add to list
+        if distance <= max_distance:
+            close_package_names.append(package)
 
-	return close_package_names
+    return close_package_names
 
 
 def createSuspiciousPackageDict(all_packages, top_packages):
-	""" Examine all top packages for typosquatters
+    """ Examine all top packages for typosquatters
 
 	Loop through all top packages and check for instances of
 	typosquatting.
@@ -170,17 +173,17 @@ def createSuspiciousPackageDict(all_packages, top_packages):
 	--suspicious_packages: an ordered dict of the top packages (key)
 	and potential typosquatters (value)
 	"""
-	suspicious_packages = collections.OrderedDict()
+    suspicious_packages = collections.OrderedDict()
 
-	for top_package in top_packages:
-		close_packages = distanceCalculations(top_package, all_packages)
-		suspicious_packages[top_package] = close_packages
+    for top_package in top_packages:
+        close_packages = distanceCalculations(top_package, all_packages)
+        suspicious_packages[top_package] = close_packages
 
-	return suspicious_packages
+    return suspicious_packages
 
 
 def storeSquattingCandidates(squat_candidates):
-	''' Persist results of squatting candidate search
+    """ Persist results of squatting candidate search
 
 	Dump typosquatter candidate list to a json file. Store
 	with time-stamped file name to results folder.
@@ -188,17 +191,17 @@ def storeSquattingCandidates(squat_candidates):
 	INPUT:
 	--squat_candidates: A dic of the top packages and their
 	potential typosquatters
-	'''
+	"""
 
-	timestamp = time.strftime("%d-%b-%Y-%H-%M-%S", time.localtime())
-	full_file_name = timestamp + "-record" + ".json"
-	file_name = os.path.join("results", full_file_name)
-	with open(file_name, 'w') as path:
-   		json.dump(squat_candidates, path)
+    timestamp = time.strftime("%d-%b-%Y-%H-%M-%S", time.localtime())
+    full_file_name = timestamp + "-record" + ".json"
+    file_name = os.path.join("results", full_file_name)
+    with open(file_name, "w") as path:
+        json.dump(squat_candidates, path)
 
 
 def whitelist(squat_candidates, whitelist_filename="whitelist.txt"):
-	''' Remove whitelisted packages from typosquat candidate list
+    """ Remove whitelisted packages from typosquat candidate list
 
 	Using packages listed in the whitelist_filename file, remove all
 	potential typosquatters that are found in the whitelist, a list of
@@ -210,41 +213,42 @@ def whitelist(squat_candidates, whitelist_filename="whitelist.txt"):
 
 	OUPUT:
 	--dict of packages and post-whitelist potential typosquatters
-	'''
+	"""
 
-	# Create whitelist
-	whitelist = []
-	with open(whitelist_filename, 'r') as file:
-		for line in file:
-			# Strip out end of line character
- 			whitelist.append(line.strip('\n'))
+    # Create whitelist
+    whitelist = []
+    with open(whitelist_filename, "r") as file:
+        for line in file:
+            # Strip out end of line character
+            whitelist.append(line.strip("\n"))
 
- 	# Remove packages contained in whitelist from dict of
- 	# top packages (keys) and potential typosquatters (values)
-	for pkg in squat_candidates: # loop thru pkg's
-		new_squat_candidates = []
-		for candidate in squat_candidates[pkg]:
-			# Only keep candidates NOT on whitelist
-			if candidate not in whitelist:
-				new_squat_candidates.append(candidate)
-		# Update typosquat candidate list
-		squat_candidates[pkg] = new_squat_candidates
+        # Remove packages contained in whitelist from dict of
+        # top packages (keys) and potential typosquatters (values)
+    for pkg in squat_candidates:  # loop thru pkg's
+        new_squat_candidates = []
+        for candidate in squat_candidates[pkg]:
+            # Only keep candidates NOT on whitelist
+            if candidate not in whitelist:
+                new_squat_candidates.append(candidate)
+        # Update typosquat candidate list
+        squat_candidates[pkg] = new_squat_candidates
 
-	return squat_candidates
+    return squat_candidates
 
-if __name__ == '__main__':
 
-	current_timestamp, package_names = getAllPackages()
-	top_packages = getTopPackages()
-	filtered_package_list = filterByPackageNameLen(top_packages)
-	squat_candidates = createSuspiciousPackageDict(package_names, filtered_package_list)
-	whitelist_candidates = whitelist(squat_candidates)
-	storeSquattingCandidates(whitelist_candidates)
+if __name__ == "__main__":
 
-	# Print all top packages and potential typosquatters
-	print("Number of top packages to examine: " + str(len(squat_candidates)))
-	cnt_potential_squatters = 0
-	for i in whitelist_candidates:
-		print(i, ': ', whitelist_candidates[i])
-		cnt_potential_squatters += len(whitelist_candidates[i])
-	print("Number of potential typosquatters: " + str(cnt_potential_squatters))
+    current_timestamp, package_names = getAllPackages()
+    top_packages = getTopPackages()
+    filtered_package_list = filterByPackageNameLen(top_packages)
+    squat_candidates = createSuspiciousPackageDict(package_names, filtered_package_list)
+    whitelist_candidates = whitelist(squat_candidates)
+    storeSquattingCandidates(whitelist_candidates)
+
+    # Print all top packages and potential typosquatters
+    print("Number of top packages to examine: " + str(len(squat_candidates)))
+    cnt_potential_squatters = 0
+    for i in whitelist_candidates:
+        print(i, ": ", whitelist_candidates[i])
+        cnt_potential_squatters += len(whitelist_candidates[i])
+    print("Number of potential typosquatters: " + str(cnt_potential_squatters))
