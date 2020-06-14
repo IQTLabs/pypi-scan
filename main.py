12d58c525ee563984b625518a349196fd0b51bac
jspeed-meyers <johnmeyersster@gmail.com>
2020-06-14 05:33:33 -0700

    Add CLI and 1 module scan capability

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """
-Check top pypi packages for typosquatting
+Pypi typosquatting scanning capability
+
+Rewrite to make clear this is a bunch of related functionality
 
 This programs checks the top packages (the default is the top 50)
 for typosquatting, i.e. instances of other packages that are intentionally
@@ -7,22 +9,41 @@ named such that common mis-typings of the original package could result in
 typing this other package name. Mis-typing distance is measured via
 levenshtein distance, a measure of "edit" distance. The default configuration
 identifies a package as a potential typosquatter if its edit distance is less
-than or equal a specified value (default is 1 ) compared to one of the top
+than or equal a specified value (default is 1) compared to one of the top
 packages. Additionally, there is a whitelist capability to exclude packages
 that are known good. Note: Only packages whose names are at least as long a
 specified minimum are analyzed.
 """
 
+import argparse
+
 from filters import filterByPackageNameLen, distanceCalculations, whitelist
 from scrapers import getAllPackages, getTopPackages
 from utils import createSuspiciousPackageDict, storeSquattingCandidates
 
-if __name__ == "__main__":
+def parseArgs():
+    """ Parse command line arguments. """
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--operation",
+                        help="Specify operation to perform.",
+                        choices=['mod-squatters', 'top-mods'],
+                        default='mod-squatters')
+    parser.add_argument("-m", "--module_name",
+                        help="Module name to check for typosquatters.")
+    args = parser.parse_args()
+
+    return args
+
+def topMods():
+    """ Check top packages for typosquatters """
+
+    # Get list of potential typosquatters
     package_names = getAllPackages()
     top_packages = getTopPackages()
     filtered_package_list = filterByPackageNameLen(top_packages)
     squat_candidates = createSuspiciousPackageDict(package_names, filtered_package_list)
+    # TODO: This whitelist candidate naming is confusing. Should probably change.
     whitelist_candidates = whitelist(squat_candidates)
     storeSquattingCandidates(whitelist_candidates)
 
@@ -33,3 +54,27 @@ if __name__ == "__main__":
         print(i, ": ", whitelist_candidates[i])
         cnt_potential_squatters += len(whitelist_candidates[i])
     print("Number of potential typosquatters: " + str(cnt_potential_squatters))
+
+def modSquatters(module):
+    """ Check if a particular package name has potential squatters"""
+
+    module_in_list = [module]
+
+    package_names = getAllPackages()
+    squat_candidates = createSuspiciousPackageDict(package_names,
+                                                   module_in_list)
+
+    print('Checking ' + module + ' for typosquatting candidates.')
+    for i, candidate in enumerate(squat_candidates[module]):
+            print(str(i) + ": " + candidate)
+
+if __name__ == "__main__":
+
+    cli_args = parseArgs() # get command line arguments
+
+    # Check top packages for typosquatters
+    if cli_args.operation == "top-mods":
+        topMods()
+    elif cli_args.operation == "mod-squatters":
+        #TODO: Make specifying edit distance a choice
+        modSquatters(cli_args.module_name)
