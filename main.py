92e7545c876fb71b3382c99a54a82abef59ddb3e
jspeed-meyers <johnmeyersster@gmail.com>
2020-06-15 04:58:51 -0700

    Update main.py documentation

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 """
 Pypi typosquatting scanning capability
 
-Rewrite to make clear this is a bunch of related functionality
+This program contains functionality related to scanning pypi, the python
+package index, for typosquatting. Typosquatting occurs when there are packages
+that are intentionally named such that common mis-typings of the original
+package could result in typing this other package name. Mis-typing distance
+is measured via levenshtein distance, a measure of "edit" distance.
 
-This programs checks the top packages (the default is the top 50)
-for typosquatting, i.e. instances of other packages that are intentionally
-named such that common mis-typings of the original package could result in
-typing this other package name. Mis-typing distance is measured via
-levenshtein distance, a measure of "edit" distance. The default configuration
-identifies a package as a potential typosquatter if its edit distance is less
-than or equal a specified value (default is 1) compared to one of the top
-packages. Additionally, there is a whitelist capability to exclude packages
-that are known good. Note: Only packages whose names are at least as long a
-specified minimum are analyzed.
+One functionality allows user to specify a package name and to see if there
+are any other packages that are similarly named. This could help a package
+creator or maintainer check for possible typosquatting.
+
+There is also a functionality better suited for the administrators of pypi
+or for an information security researcher: this program can check the top
+packages (the default is the top 50) for typosquatting. The default
+configuration identifies a package as a potential typosquatter if its edit
+distance is less than or equal a specified value (default is 1) compared to
+one of the top packages. Additionally, there is a whitelist capability to
+exclude packages that are known good. Note: Only packages whose names are at
+least as long a specified minimum are analyzed.
 """
 
 import argparse
@@ -63,7 +69,7 @@ def modSquatters(module):
                                                    module_in_list)
     # Print results
     print('Checking ' + module + ' for typosquatting candidates.')
-    # Check if there are not typosquatting candidates
+    # Check for no typosquatting candidates
     if len(squat_candidates[module]) == 0:
         print('No typosquatting candidates found.')
     else:
