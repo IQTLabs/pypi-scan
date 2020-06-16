8775cf2a7ffbf04dd68361478041ee24896d61c9
jspeed-meyers <johnmeyersster@gmail.com>
2020-06-16 04:05:40 -0700

    Add constants file for easy configuration

---
+++ constants.py
@@ -0,0 +1,14 @@
+"""
+Constants
+
+This file consolidates all constants into one module.
+"""
+
+# Number of top packages on pypi to scan
+TOP_N = 50
+
+# Edit distance threshold to determine typosquatting status
+MAX_DISTANCE = 1
+
+# Minimum length of package name to be included for analysis
+MIN_LEN_PACKAGE_NAME = 5
